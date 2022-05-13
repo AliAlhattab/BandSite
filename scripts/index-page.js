9f67b64b46a59ae49131b3c63f9eac42ea678b60
AliAlhattab <Ali.Alhattab@hotmail.com>
2022-05-13 17:01:28 -0400

    changed my for loop into a forEach

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -15,40 +15,35 @@ let commentSection = [
         comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
     }
 ]
-
+// selecting the class .form__comments
 const container = document.querySelector('.form__comments');
 
-function makeComment(comment){
-const commentContainer = document.createElement("div");
-const commentNameDate = document.createElement("div");
-const commentPicture = document.createElement('div');
-const commentInfo = document.createElement('div');
-const commentText = document.createElement('p');
-const commentName = document.createElement('p');
-const commentDate = document.createElement('p');
-
-commentContainer.classList.add("form__container");
-commentNameDate.classList.add("form__name-date");
-commentText.classList.add("form__paragraph");
-commentName.classList.add("form__name");
-commentDate.classList.add("form__date");
-commentPicture.classList.add("form__picture");
-commentInfo.classList.add("form__comment-info");
-
-commentName.innerText = comment.name;
-commentDate.innerText = comment.date;
-commentText.innerText = comment.comment;
-
-commentNameDate.appendChild(commentName);
-commentNameDate.appendChild(commentDate);
-commentContainer.appendChild(commentPicture);
-commentContainer.appendChild(commentInfo);
-commentInfo.appendChild(commentNameDate);
-commentInfo.appendChild(commentText);
-container.appendChild(commentContainer);
-
-}
-
-for (i = 0; i < commentSection.length; i++){
-    makeComment(commentSection[i]);
-}
+commentSection.forEach((comment) => {
+    const commentContainer = document.createElement("div");
+    const commentNameDate = document.createElement("div");
+    const commentPicture = document.createElement('div');
+    const commentInfo = document.createElement('div');
+    const commentText = document.createElement('p');
+    const commentName = document.createElement('p');
+    const commentDate = document.createElement('p');
+    
+    commentContainer.classList.add("form__container");
+    commentNameDate.classList.add("form__name-date");
+    commentText.classList.add("form__paragraph");
+    commentName.classList.add("form__name");
+    commentDate.classList.add("form__date");
+    commentPicture.classList.add("form__picture");
+    commentInfo.classList.add("form__comment-info");
+    
+    commentName.innerText = comment.name;
+    commentDate.innerText = comment.date;
+    commentText.innerText = comment.comment;
+    
+    commentNameDate.appendChild(commentName);
+    commentNameDate.appendChild(commentDate);
+    commentContainer.appendChild(commentPicture);
+    commentContainer.appendChild(commentInfo);
+    commentInfo.appendChild(commentNameDate);
+    commentInfo.appendChild(commentText);
+    container.appendChild(commentContainer); 
+});
